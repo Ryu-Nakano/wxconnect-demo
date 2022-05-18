46bb63123168391d53d91bdf6e321c8c14eb5c48
ryu <55970532+Ryu-Nakano@users.noreply.github.com>
2022-05-18 17:17:43 +0900

    Update webexcc.js

--- js/webexcc.js
+++ js/webexcc.js
@@ -29,7 +29,7 @@ async function sendRequest(bodyText) {
 		"Phone":"09012345678"
 	});
 	
-	var raw = '{"destination":"+14402079228", "Lastname":"' + ln + '","Firstname":"' + fn + '", "Email":"' + ma + '", "Phone":"' + pn + '"}';
+	var raw = '{"destination":"+15162001844", "Lastname":"' + ln + '","Firstname":"' + fn + '", "Email":"' + ma + '", "Phone":"' + pn + '"}';
 	console.log(raw);
 	
 	var requestOptions = {
