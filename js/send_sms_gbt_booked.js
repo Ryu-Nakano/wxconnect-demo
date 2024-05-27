fd0526359dabf53a4f29b7e840195273827eca0a
ryu <55970532+Ryu-Nakano@users.noreply.github.com>
2024-05-27 18:51:54 +0900

    airline ticket
    airline ticket

---
+++ js/send_sms_gbt_booked.js
@@ -0,0 +1,52 @@
+// 変数定義
+// なし
+
+
+/*-----------------------------------------------------------*/
+/* sendRequest 
+/*-----------------------------------------------------------*/
+// 引数（IN）
+//  無し
+// 戻り値（OUT）
+//  0 (固定)
+/*-----------------------------------------------------------*/
+async function sendRequest() {
+	
+	// リクエスト作成
+	var myHeaders = new Headers();
+	myHeaders.append("Content-Type", "application/json");
+
+	const comn = document.getElementById("companyName").value;
+	const cusn = document.getElementById("customerName").value;
+	const email = document.getElementById("Email").value;
+	const phone = document.getElementById("Phone").value;
+
+// JSON Sample
+//{
+//    "CompanyName": "シスコシステムズ",
+//    "CustomerName": "Ryu Nakano",
+//    "Email": "rynakano@cisco.com",
+//    "Phone": "+818034779718"
+//}
+	
+	var raw = `{"CompanyName":"${comn}", "CustomerName":"${cusn}","Email":"${email}", "Phone":"${phone}"}`;
+	console.log(raw);
+	
+	var requestOptions = {
+		method: 'POST',
+		headers: myHeaders,
+		body: raw,
+		redirect: 'follow'
+	};
+	
+	// リクエスト送信
+	const response = await fetch("https://hooks.us.webexconnect.io/events/P3SXZA270B", requestOptions);
+	const links = await response.json();
+	console.log(links);
+	
+	var btnArea = document.getElementById("btnSend");
+	btnArea.innerHTML='SMSを送信しました';
+	
+	return 0;
+}
+
